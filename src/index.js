00bd2923e1e2311764b3b6f4cc3cdce87aa95bce
zhanghua8003 <zhanghua8003@gmail.com>
2017-03-11 22:09:48 +0800

    插件

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {Provider} from 'react-redux';
+
+import {Router, browserHistory, hasHistory} from 'react-router'
+
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider >
+    <Router history={hasHistory} />
+  </Provider>,
   document.getElementById('root')
 );
